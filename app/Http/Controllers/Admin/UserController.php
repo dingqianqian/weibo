d1e791b35260a5a8bba75d059163b1e147ddb0f7
dingianqian <2842961733@qq.com>
2018-04-10 13:51:04 +0800

    dqq

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Model\User;
+use App\Http\Model\User_info;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+
+//        dd($_SERVER);
+        //去除二边的空格 判断
+        $key = trim($request->input('keywords',''));
+        //查询出二个表的所有数据
+        $user = User::join('user_infos','users.uid','=','user_infos.uid')
+                ->where('realname','like',"%".$key."%")
+                ->paginate(3);
+        //把数据传给模板
+        return view('admin.user.index',compact('user','key'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.user.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/index/welcome.blade.php
@@ -0,0 +1,55 @@
+﻿@extends('admin.layout.blank')
+
+@section('title')
+<title>我的桌面</title>
+@endsection
+
+@section('body')
+
+
+<div class="page-container">
+	<p class="f-20 text-success">lamp182-微博<span class="f-14"> </span>后台首页</p>
+
+	<table class="table table-border table-bordered table-bg mt-20">
+		<thead>
+			<tr>
+				<th colspan="2" scope="col">服务器信息</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>服务器IP地址</td>
+				<td>{{$_SERVER['SERVER_ADDR']}}</td>
+			</tr>
+
+			<tr>
+				<td>服务器域名</td>
+				<td>{{$_SERVER['SERVER_NAME']}}</td>
+			</tr>
+			<tr>
+				<td>服务器端口 </td>
+				<td>{{$_SERVER['SERVER_PORT']}}</td>
+			</tr>
+
+			<tr>
+				<td>服务器操作系统 </td>
+				<td>{{$_SERVER['SERVER_SOFTWARE']}}</td>
+			</tr>
+
+			<tr>
+				<td>服务器的语言种类 </td>
+				<td>{{$_SERVER['HTTP_ACCEPT_LANGUAGE']}}</td>
+			</tr>
+
+			<tr>
+				<td>服务器当前时间 </td>
+				{{--<td>{{$_SERVER['REQUEST_TIME_FLOAT']}}</td>--}}
+				<td>{{  date('Y年m月d日 H:i:s',time()) }}</td>
+			</tr>
+
+		</tbody>
+	</table>
+</div>
+
+
+@endsection
